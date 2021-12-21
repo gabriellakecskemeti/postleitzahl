59c532bc5457685633c77eabfad765d0e5acc531
gabriella.kecskemeti <gabriella.kecskemeti@chello.at>
2021-12-21 22:34:49 +0100

    first try

--- personal_data_administrator.py
+++ personal_data_administrator.py
@@ -1,2 +1,4 @@
 import validators
-print(validators.input_postal_code("Please enter your postal code (Enter 'q' to quit): ", 1010, 1020, 1030))
+
+
+print(validators.input_postal_code("Please enter your postal code (Enter 'q' to quit): ", 1010, 1020, 1030, 1040, 1050))
--- validators.py
+++ validators.py
@@ -1,18 +1,22 @@
 def input_postal_code(prompt, *postalcodes):
     number = 0
+    s_postalcodes = set(postalcodes)
     while True:
-        try:
             number_char = input(f"{prompt}")
-            # if Q  oder q
-
-            # number= char in zahl  umwandeln
-            # *postalcodes schauen
-            break
-        except ValueError:
-            print("Only numbers, please!")
-
-    return number
+            if number_char.upper() == "Q":
+                return "Good bye!"
+            number = _check_postalcode(number_char, s_postalcodes)
+            if number is not None:
+                return number
 
 
 def _check_postalcode(value, postalcodes):
-    print("hello")
+    try:
+        number = int(value)
+    except ValueError:
+        print("Only numbers, please!")
+        return
+    for x in postalcodes:
+        if number == x:
+            return number
+    return
