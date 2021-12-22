321e8b3edb1c3fdafdf649d2909991e7b5185c3d
gabriella.kecskemeti <gabriella.kecskemeti@chello.at>
2021-12-22 11:14:07 +0100

    zA 5.teil

--- validators.py
+++ validators.py
@@ -5,18 +5,19 @@ def input_postal_code(prompt, *postalcodes):
         if number_char.upper() == "Q":
             return "Good bye!"
 
-        if _check_postalcode(number_char, postalcodes) is not None:
-            return number_char
+        try:
+            return _check_postalcode(number_char, postalcodes)
+        except ValueError as error:
+            print(error)
 
 
 def _check_postalcode(value, postalcodes):
     try:
         number = int(value)
     except ValueError:
-        print(f"{value} is not numerical! Only numbers, please!")
-        return
-    try:
-        return postalcodes.index(number)
-    except ValueError:
-        print(f"Postal code {number} is not valid! Valid codes are: {postalcodes}")
-        return
+        raise ValueError(f"{value} is not numerical! Only numbers, please!")
+
+    if number not in postalcodes:
+        raise ValueError(f"Postal code {number} is not valid! Valid codes are: {postalcodes}")
+
+    return number
