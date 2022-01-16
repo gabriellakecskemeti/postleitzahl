3ca55e9c727172b327e6dd5ebce7dec17922dad3
gabriella.kecskemeti <gabriella.kecskemeti@chello.at>
2022-01-16 18:39:55 +0100

    test branch

--- personal_data_administrator.py
+++ personal_data_administrator.py
@@ -1,3 +1,5 @@
 import validators
 
 print(validators.input_postal_code("Please enter your postal code (Enter 'q' to quit): ", 1010, 1020, 1030, 1040, 1050))
+print("master")
+print("testbranch")
--- validators.py
+++ validators.py
@@ -4,7 +4,6 @@ def input_postal_code(prompt, *postalcodes):
         number_char = input(prompt)
         if number_char.upper() == "Q":
             return "Good bye!"
-
         try:
             return _check_postalcode(number_char, postalcodes)
         except ValueError as error:
