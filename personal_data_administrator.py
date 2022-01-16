cf0067d35e1c32f76ca801db2be2e3f97841f0ef
gabriella.kecskemeti <gabriella.kecskemeti@chello.at>
2022-01-16 18:49:21 +0100

    rebase testbranch to master

--- personal_data_administrator.py
+++ personal_data_administrator.py
@@ -3,3 +3,5 @@ import validators
 print(validators.input_postal_code("Please enter your postal code (Enter 'q' to quit): ", 1010, 1020, 1030, 1040, 1050))
 
 print("testbranch")
+
+print("rebase testbranch to master")
